df033f1f79328ae066499acdeb5f5f589a1aa10e
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-05 03:36:39 +0545

    setup for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/product-list-with-cart/',
   plugins: [vue()],
   server: {
     port: 3000,
